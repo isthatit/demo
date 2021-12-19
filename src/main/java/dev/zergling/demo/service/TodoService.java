0b846c810f7b6855d8512244bfd248b52fa8928b
MagicalFlutter <flutter@MagicalFlutterui-MacBookPro.local>
2021-12-19 13:27:23 +0900

    CRUD REST API 완성

--- src/main/java/dev/zergling/demo/service/TodoService.java
+++ src/main/java/dev/zergling/demo/service/TodoService.java
@@ -1,6 +1,7 @@
 package dev.zergling.demo.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -37,6 +38,42 @@ public class TodoService {
 		
 		return repository.findByUserId(entity.getUserId());
 	}
+	
+	
+	public List<TodoEntity> retrieve(final String userId) {
+		return repository.findByUserId(userId);
+	}
+	
+	public List<TodoEntity> update(final TodoEntity entity) {
+		validate(entity);
+		
+		final Optional<TodoEntity> original  = repository.findById(entity.getId());
+		
+		original.ifPresent(todo -> {
+			todo.setTitle(entity.getTitle());
+			todo.setDone(entity.isDone());
+		});
+		
+		return retrieve(entity.getUserId());
+	}
+	
+	public List<TodoEntity> delete(final TodoEntity entity) {
+		validate(entity);
+		
+		try {
+			repository.delete(entity);
+		} catch (Exception e) {
+			log.error("error deleting entity ", entity.getId(), e);
+			
+			throw new RuntimeException("error deleting entity " + entity.getId());
+		}
+		
+		return retrieve(entity.getUserId());
+	}
+	
+	
+	
+	
 	
 	
 	private void validate(final TodoEntity entity) {
